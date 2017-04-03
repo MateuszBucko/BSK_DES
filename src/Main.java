fb39ac321a571ec3b68a10da8903cdf23aeedea0
Mateusz Bućko <mateuszbucko1@gmail.com>
2017-04-03 21:55:58 +0200

    full crypting DES

--- src/Main.java
+++ src/Main.java
@@ -79,8 +79,9 @@ public class Main {
             for (int i = 0; i < permuted.length; i++) {
                 System.out.print(permuted[i]);
             }
+            mergedKeysMap.put(k, permuted);
         });
-
+        System.out.println();
         System.out.println("Dane wejściowe: ");
         for (int i = 0; i < inputArray.length; i++) {
             System.out.print(inputArray[i]);
@@ -119,9 +120,197 @@ public class Main {
             }
         }
 
-        
+        int[] L, R;
+        L = new int[32];
+        R = new int[32];
+        //wstawienie w tablice L i R wszystkie bity z tablic L0, R0 wygenerowanych na podstawie danych wejściowych (po 32 bity każda)
+        for (int i = 0; i < L0.length; i++) {
+            L[i] = L0[i];
+            R[i] = R0[i];
+        }
 
 
+        System.out.println();
+        System.out.print("Przejście przez permutację E i funkcję F");
+        for (int i = 0; i < 16; i++) {
+            //permutacja E tablic
+            //L = permute(L, Tables.E);
+
+            int[] permutedR = permute(R, Tables.E);
+
+            System.out.println();
+            System.out.print("Tablica R po permutacji E");
+            System.out.println();
+            for (int k = 0; k < permutedR.length; k++) {
+                System.out.print(permutedR[k]);
+                if (k % 6 == 5 && k != 0) {
+                    System.out.print(" ");
+                }
+            }
+
+            int[] xored_R = new int[permutedR.length];
+            int[] fKey = mergedKeysMap.get("K" + (i + 1));
+
+
+            System.out.println();
+            System.out.print("Klucz");
+            System.out.println();
+            for (int k = 0; k < fKey.length; k++) {
+                System.out.print(fKey[k]);
+                if (k % 6 == 5 && k != 0) {
+                    System.out.print(" ");
+                }
+            }
+
+
+            //xorowanie tablicy R z kluczem
+            for (int j = 0; j < permutedR.length; j++) {
+                xored_R[j] = (permutedR[j] + fKey[j]) % 2;
+            }
+            System.out.println();
+            System.out.print("Tablica zXORowana z kluczem");
+            System.out.println();
+            for (int l = 0; l < xored_R.length; l++) {
+                System.out.print(xored_R[l]);
+                if (l % 6 == 5 && l != 0) {
+                    System.out.print(" ");
+                }
+            }
+
+            //przetworzenie tablicy R przez funkcję f
+            int[] afterF = functionF(xored_R);
+            System.out.println();
+            System.out.print("Wynik F: ");
+            System.out.println();
+            for (int l = 0; l < afterF.length; l++) {
+                System.out.print(afterF[l]);
+                if (l % 4 == 3 && l != 0) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            System.out.print("Wynik F po permutacji P: ");
+            System.out.println();
+            afterF = permute(afterF, Tables.P);
+            for (int l = 0; l < afterF.length; l++) {
+                System.out.print(afterF[l]);
+                if (l % 4 == 3 && l != 0) {
+                    System.out.print(" ");
+                }
+            }
+
+
+            int[] resultTab = new int[32];
+            for (int j = 0; j < afterF.length; j++) {
+
+                resultTab[j] = (afterF[j] + L[j]) % 2;
+            }
+
+            System.arraycopy(R, 0, L, 0, L.length);
+            R = resultTab;
+
+
+            System.out.println();
+            System.out.print("L:" + i);
+            System.out.println();
+            for (int l = 0; l < L.length; l++) {
+                System.out.print(L[l]);
+                if (l % 4 == 3 && l != 0) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            System.out.print("R:" + i);
+            System.out.println();
+            for (int l = 0; l < R.length; l++) {
+                System.out.print(R[l]);
+                if (l % 4 == 3 && l != 0) {
+                    System.out.print(" ");
+                }
+            }
+
+
+        }
+
+        int[] finalLandRArray = new int[64];
+        for (int i = 0; i < 32; i++) {
+            finalLandRArray[i] = R[i];
+        }
+        int wsk = 0;
+        for (int i = 32; i < 64; i++) {
+            finalLandRArray[i] = L[wsk];
+            wsk++;
+        }
+
+        System.out.println();
+        System.out.print("R16L16:  ");
+        System.out.println();
+        for (int l = 0; l < finalLandRArray.length; l++) {
+            System.out.print(finalLandRArray[l]);
+            if (l % 8 == 7 && l != 0) {
+                System.out.print(" ");
+            }
+        }
+
+        int[] finalArrayPermuted = permute(finalLandRArray, Tables.FP);
+        System.out.println();
+        System.out.print("Finalna tablica:  ");
+        System.out.println();
+        for (int l = 0; l < finalArrayPermuted.length; l++) {
+            System.out.print(finalArrayPermuted[l]);
+            if (l % 8 == 7 && l != 0) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        System.out.print("Wynik szyfrowania: ");
+        System.out.println();
+        System.out.print(bitArrayToHexText(finalArrayPermuted));
+
+
+    }
+
+    public static int[] functionF(int[] array) {
+        int[] result = new int[32];
+        List<int[]> splitedList = splitIntoSixBitsArray(array);
+        //8 iteracji po tablicach od S1 do S8 (S0 - S8)
+        int wsk = 0;
+        for (int i = 0; i < 8; i++) {
+            int[] tab = splitedList.get(i);
+
+            String row = String.valueOf(tab[0]) + String.valueOf(tab[5]);
+            String col = String.valueOf(tab[1]) + String.valueOf(tab[2]) + String.valueOf(tab[3]) + String.valueOf(tab[4]);
+            int rowPos = Integer.parseInt(row, 2) * 16;
+            int colPos = Integer.parseInt(col, 2);
+            int pos = rowPos + colPos;
+            pos = Tables.S[i][pos];
+
+            String bin = Integer.toString(pos, 2);
+
+            while (bin.length() < 4) {
+                bin = "0" + bin;
+            }
+
+            for (int k = 0; k < bin.length(); k++) {
+                result[wsk] = Integer.valueOf(bin.substring(k, k + 1));
+                wsk++;
+            }
+
+
+        }
+        return result;
+    }
+
+    public static List<int[]> splitIntoSixBitsArray(int[] array) {
+        List<int[]> list = new ArrayList<>();
+        int wsk = 0;
+
+        for (int i = 1; i <= 8; i++) {
+            int temp[] = new int[6];
+            System.arraycopy(array, (i - 1) * 6, temp, 0, 6);
+            list.add(temp);
+        }
+        return list;
     }
 
     public static int[] hexTextToBitArray(String hexText) {
@@ -140,6 +329,26 @@ public class Main {
             }
         }
         return array;
+    }
+
+    public static String bitArrayToHexText(int[] array) {
+        String hexText = "";
+        List<int[]> listOfEightsBits = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            listOfEightsBits.add(split(array, i * 8, i * 8 + 8));
+        }
+
+        for (int[] list : listOfEightsBits) {
+            StringBuilder stringBuilder = new StringBuilder(list.length);
+            for (int i = 0; i < list.length; i++) {
+                stringBuilder.append(list[i]);
+            }
+            int res = Integer.valueOf(stringBuilder.toString(), 2);
+            hexText += String.format("%02X", res & 0xFFFFF);
+        }
+
+
+        return hexText;
     }
 
     public static int[] permute(int[] array, byte[] permuteTable) {
